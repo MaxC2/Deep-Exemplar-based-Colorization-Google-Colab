6682ec794dbda191a40423ecb2faad5493f4c3f1
Max C2 <maxprod2020@gmail.com>
2020-10-22 20:26:23 +0200

    prebuilt

--- demo/run-user.sh
+++ demo/run-user.sh
@@ -12,4 +12,5 @@ bin/deep_image_analogy models/deep_image_analogy/ $EXAMPLE 0 $NUM_LINES 0
 
 bin/similarity_combo models/similarity_subnet/ $EXAMPLE 0 $NUM_LINES 0
 
-python3 ../colorization_subnet/test.py --short_size 256 --test_model models/colorization_subnet/example_net.pth --data_root $EXAMPLE --out_dir $EXAMPLE/res/ --gpu_id 0
+# python3 ../colorization_subnet/test.py --short_size 232 --test_model models/colorization_subnet/example_net.pth --data_root $EXAMPLE --out_dir $EXAMPLE/res/ --gpu_id 0
+python3 ../colorization_subnet/test.py --short_size 464 --test_model models/colorization_subnet/example_net.pth --data_root $EXAMPLE --out_dir $EXAMPLE/res/ --gpu_id 0
