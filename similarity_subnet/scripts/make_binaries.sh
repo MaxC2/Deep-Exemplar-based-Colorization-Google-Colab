1cfc228488b4b3d90c42c778fa9f152cc400eda2
Max C2 <maxprod2020@gmail.com>
2020-10-22 16:20:56 +0200

    Update make_binaries.sh

--- similarity_subnet/scripts/make_binaries.sh
+++ similarity_subnet/scripts/make_binaries.sh
@@ -13,13 +13,13 @@ CUDA="-arch=sm_52 \
 # Make Similarity Combo
 SOURCE=similarity_subnet/linux/similarity_combo/source
 
-nvcc $CUDA $SOURCE/*.cu $SOURCE/*.cpp -I/caffe/include -I/caffe/build/src -L/caffe/build/lib -o demo/bin/similarity_combo -std=c++11 \
+nvcc $CUDA $SOURCE/*.cu $SOURCE/*.cpp -Icaffe/include -Icaffe/build/src -Lcaffe/build/lib -o demo/bin/similarity_combo -std=c++11 \
     -I/usr/include/eigen3 -lopencv_core -lopencv_highgui -lopencv_imgproc \
     -lopencv_imgcodecs -lboost_system -lboost_filesystem -lcublas -lcaffe -lglog
 
 # Make Deep Image Analogy
 SOURCE=similarity_subnet/linux/deep_image_analogy/source
 
-nvcc $CUDA $SOURCE/*.cu $SOURCE/*.cpp -I/caffe/include -I/caffe/build/src -L/caffe/build/lib -o demo/bin/deep_image_analogy -std=c++11 \
+nvcc $CUDA $SOURCE/*.cu $SOURCE/*.cpp -Icaffe/include -Icaffe/build/src -Lcaffe/build/lib -o demo/bin/deep_image_analogy -std=c++11 \
     -I/usr/include/eigen3 -lopencv_core -lopencv_highgui -lopencv_imgproc \
     -lopencv_imgcodecs -lboost_system -lboost_filesystem -lcublas -lcaffe -lglog
